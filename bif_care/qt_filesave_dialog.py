40e830618ef64d7332e26034fa1c164652e1f893
Christoph Sommer <christoph.sommer@ist.ac.at>
2019-03-14 13:47:09 +0100

    added missing qt dialog files

---
+++ bif_care/qt_filesave_dialog.py
@@ -0,0 +1,17 @@
+from sys import executable, argv
+from subprocess import check_output
+from PyQt5.QtWidgets import QFileDialog, QApplication
+
+def gui_fsavename(directory='./'):
+    """Open a file dialog, starting in the given directory, and return
+    the chosen filename"""
+    # run this exact file in a separate process, and grab the result
+    file = check_output([executable, __file__, directory])
+    return file.strip().decode("utf-8")     
+
+if __name__ == "__main__":
+    directory = argv[1]
+    app = QApplication([directory])
+    fname = QFileDialog.getSaveFileName(None, "Create a new file...", 
+            directory, filter="BifCare (*.json)")
+    print(fname[0])
